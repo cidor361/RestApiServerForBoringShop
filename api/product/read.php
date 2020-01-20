f4732e14eecf1c323a226a7ca09d81213b2f28ad
Igor Grebennikov <cidor361@gmail.com>
2020-01-21 01:58:15 +0300

    I

--- api/product/read.php
+++ api/product/read.php
@@ -8,7 +8,7 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 include_once '../config/database.php';
 include_once '../objects/product.php';
 
-$num = $_POST['num'];
+$num = $_GET['num'];
 echo var_dump($num);
 
 $db = new Database();
